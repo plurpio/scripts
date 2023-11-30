8804c8110bce047cba4b459bfec0abc9693a9c22
plurpio <nicolaialassov@icloud.com>
2023-11-30 22:13:41 +1100

    Removed testing URL

--- scamSpam/main.py
+++ scamSpam/main.py
@@ -3,7 +3,6 @@ import random
 requestssent = 0
 
 scamurl = input("Enter URL to website that is scamming, replace the email with ||EMAIL||\n> ")
-#scamurl = "https://www.rewardsgiant-au.com/svcg.aspx?SvcTP=3&apikey=6C0E62C4-FB5C-42A5-9D3B-10457F314A81&pID=31&params=email="
 
 while True:
     email = str(random.randrange(1000000000))+"@gmail.com"
