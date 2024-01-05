c72d06138fd5af04e49b79b19760e38c2f9c02c8
Plurpio <nicolaialassov@icloud.com>
2024-01-05 17:12:35 +1100

    Added virtcli

---
+++ virtcli/virt.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+# Virtual machine creator
+choice=$(echo -e "Add\n$(virsh list --all --name)" | wofi --dmenu --prompt="Select a virtual machine: ")
+
+case "$choice" in
+  Add)
+    kitty -e ~/scripts/virtcli/virtAdd.sh
+    remote-viewer spice://127.0.0.1:5900
+    ;;
+  *)
+    virsh -q start $choice
+    remote-viewer spice://127.0.0.1:5900
+    ;;
+esac
